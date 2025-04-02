fe3ed6840c6abe51c9b040f5815309cc49bd0cc9
Van Nguyen <vdnnguyen94@gmail.com>
2025-04-01 20:29:44 -0400

    page routing

--- VanNguyen_SeyeonJo_ManagerClient/src/main/java/com/example/demo/ManagerController.java
+++ VanNguyen_SeyeonJo_ManagerClient/src/main/java/com/example/demo/ManagerController.java
@@ -119,7 +119,7 @@ public class ManagerController {
     }
 
     // View individual Staff details
-    @GetMapping("admin/staff/view/{staffId}")
+    @GetMapping("admin/staff/{staffId}")
     public String viewStaff(@PathVariable Integer staffId, Model model) {
         Staff staff = restTemplate.getForObject(apiURI + "/api/staff/{staffId}", Staff.class, staffId);
         model.addAttribute("staff", staff);
